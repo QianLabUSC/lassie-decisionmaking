b0a4ff4d4980c2bd5aba43f5d8d6eb0e7b9b24b0
ryoma-liu <shipengl@usc.edu>
2023-03-04 22:38:53 -0800

    white_sand_debugging

--- src/components/ClickableImage.tsx
+++ src/components/ClickableImage.tsx
@@ -1,50 +1,63 @@
-import * as React from 'react';
-import { useState, useCallback, useLayoutEffect } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { NORMALIZED_HEIGHT, POPOVER_TIME, PopboxTypeEnum, NORMALIZED_WIDTH, NUM_MEASUREMENTS,INDEX_LENGTH, SCALAR_X_VAL } from '../constants';
-import { Sample } from '../types';
-import { getNearestIndex, getMeasurements } from '../util';
-import { useStateValue, Action } from '../state';
-import AddSamplePopup from './AddSamplePopup';
-import PositionIndicator from './PositionIndicator';
-import PositionIndicatorRhex from './PositionIndicatorRhex';
+import * as React from "react";
+import { useState, useCallback, useLayoutEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  NORMALIZED_HEIGHT,
+  NORMALIZED_FLAGATOB,
+  NORMALIZED_STRAT,
+  POPOVER_TIME,
+  PopboxTypeEnum,
+  NORMALIZED_WIDTH,
+  NUM_MEASUREMENTS,
+  INDEX_LENGTH,
+} from "../constants";
+import { Sample } from "../types";
+import { getNearestIndex, getMeasurements } from "../util";
+import { useStateValue, Action } from "../state";
+import AddSamplePopup from "./AddSamplePopup";
+import PositionIndicator from "./PositionIndicator";
+import PositionIndicatorRhex from "./PositionIndicatorRhex";
 
-
-const diagram = require('../../assets/diagram_scalebar.png');
+const diagram = require("../../assets/diagram_scalebar.png");
 
 const useStyles = makeStyles({
   cross: {
-    cursor: 'crosshair'
+    cursor: "crosshair",
   },
   image: {
     borderRadius: 4,
     boxShadow: "0px 2px 6px rgba(0, 0, 0, 0.3)",
-    width: '70vw',
-    display: 'block',
-    margin: 'auto',
+    width: "70vw",
+    display: "block",
+    margin: "auto",
   },
   imageDecision: {
     borderRadius: 4,
     boxShadow: "0px 2px 6px rgba(0, 0, 0, 0.3)",
-    width: '45vw',
-    display: 'block',
-    margin: 'auto',
-  }
+    width: "45vw",
+    display: "block",
+    margin: "auto",
+  },
 });
 
 interface IProps {
-  enabled: boolean,
-  addDataFunc: (sample: Sample) => void,
-  setPopOver: (popoverContent : any) => void,
-  width?: number
+  enabled: boolean;
+  addDataFunc: (sample: Sample) => void;
+  setPopOver: (popoverContent: any) => void;
+  width?: number;
 }
 
-export default function ClickableImage({ enabled, addDataFunc, setPopOver, width } : IProps) {
+export default function ClickableImage({
+  enabled,
+  addDataFunc,
+  setPopOver,
+  width,
+}: IProps) {
   const [clickPosition, setClickPosition] = useState({
     left: 0,
     top: 0,
     normOffsetX: 0,
-    normOffsetY: 0
+    normOffsetY: 0,
   });
   width = width || NORMALIZED_WIDTH;
   const [clickIndex, setClickIndex] = useState(0);
@@ -52,7 +65,7 @@ export default function ClickableImage({ enabled, addDataFunc, setPopOver, width
   // Obtain DOM element
   // It's wrong to store the height/width only
   // since the DOMRect would have 0 height initially (don't know why)
-  const imgHeightRef = useCallback((node : HTMLImageElement) => {
+  const imgHeightRef = useCallback((node: HTMLImageElement) => {
     if (node) {
       setImgEl(node);
     }
@@ -60,35 +73,43 @@ export default function ClickableImage({ enabled, addDataFunc, setPopOver, width
   const [globalState, dispatch] = useStateValue();
   const classes = useStyles();
 
-  const { samples, currUserStep, numImgClicks, transectIdx, showRobotSuggestions, showNOMInput } = globalState;
+  const {
+    samples,
+    currUserStep,
+    numImgClicks,
+    transectIdx,
+    showRobotSuggestions,
+    showNOMInput,
+  } = globalState;
   const { robotSuggestions } = currUserStep;
 
-  // Use a setTimeout method to get the height because the imgEl.getBoundingClientRect() function may return 0 for the 
-  // height if it is run immediately, which would cause all the PositionIndicator labels to initially show up at the top 
+  // Use a setTimeout method to get the height because the imgEl.getBoundingClientRect() function may return 0 for the
+  // height if it is run immediately, which would cause all the PositionIndicator labels to initially show up at the top
   // of the ClickableImage until another render.
   const [height, setHeight] = useState(0);
+
+  const mid = height - height / 2;
   setTimeout(() => {
     if (imgEl) {
-      setHeight(imgEl.getBoundingClientRect().height)
+      setHeight(imgEl.getBoundingClientRect().height);
     }
   }, 300);
   // This adjusts the PositionIndicator labels for when the window size changes
   useLayoutEffect(() => {
     function timer() {
       if (imgEl) {
-        setHeight(imgEl.getBoundingClientRect().height)
+        setHeight(imgEl.getBoundingClientRect().height);
       }
-    };
-    window.addEventListener('resize', timer);
-    return () => window.removeEventListener('resize', timer);
+    }
+    window.addEventListener("resize", timer);
+    return () => window.removeEventListener("resize", timer);
   });
 
-  
-  const onImageClick = ev => {
+  const onImageClick = (ev) => {
     if (!enabled) {
       return;
     }
-    
+
     // ev.clientX gets the x coordinate of the mouse click position on the viewport
     // ev.clientY gets the y coordinate of the mouse click position on the viewport
     const { clientX, clientY } = ev;
@@ -97,26 +118,26 @@ export default function ClickableImage({ enabled, addDataFunc, setPopOver, width
     // imgEl.getBoundingClientRect().y gets the y coordinate of the top boundary of the image
     // imgEl.getBoundingClientRect().height gets the height of the image
     const { x, y, height } = imgEl!.getBoundingClientRect();
-  
+
     // offsetX and offsetY represent the coordinates of the mouse click position with the whitespace to the left and top
     // and of the image (between the image and the edges of the viewport) removed
-    const offsetX = clientX - x; 
+    const offsetX = clientX - x;
     const offsetY = clientY - y;
-    const normOffsetX = offsetX * NORMALIZED_HEIGHT / height; 
-    const normOffsetY = offsetY * NORMALIZED_HEIGHT / height;
-    
+    const normOffsetX = (offsetX * NORMALIZED_HEIGHT) / height;
+    const normOffsetY = (offsetY * NORMALIZED_HEIGHT) / height;
+
     const index = getNearestIndex([normOffsetX, normOffsetY]);
 
     // console.log({index, offsetX, offsetY, normOffsetX, normOffsetY, height, NORMALIZED_HEIGHT, width, NORMALIZED_WIDTH}); // for debugging
 
     if (index == -1) {
-      dispatch({ 
-        type: Action.SET_SHOW_NOM_INPUT, 
-        value: false 
+      dispatch({
+        type: Action.SET_SHOW_NOM_INPUT,
+        value: false,
       });
       setPopOver({
-        content: 'Please click near the surface of stoss slope!',
-        type: PopboxTypeEnum.ERROR
+        content: "Please click near the surface of stoss slope!",
+        type: PopboxTypeEnum.ERROR,
       });
       setTimeout(() => setPopOver(null), POPOVER_TIME);
       return;
@@ -130,99 +151,109 @@ export default function ClickableImage({ enabled, addDataFunc, setPopOver, width
     // });
 
     if (numImgClicks > 0) {
-      dispatch({ 
-        type: Action.DELETE_SAMPLE, 
-        value: samples.length - 1 
+      dispatch({
+        type: Action.DELETE_SAMPLE,
+        value: samples.length - 1,
       }); // delete the old row
     }
 
-    dispatch({ 
-      type: Action.SET_NUM_IMG_CLICKS, 
-      value: numImgClicks + 1 
+    dispatch({
+      type: Action.SET_NUM_IMG_CLICKS,
+      value: numImgClicks + 1,
     });
 
-    const { shearValues, moistureValues } = getMeasurements(globalState, transectIdx, index, NUM_MEASUREMENTS);
-    const newSample : Sample = {
+    const { shearValues, moistureValues } = getMeasurements(
+      globalState,
+      transectIdx,
+      index,
+      NUM_MEASUREMENTS
+    );
+    const newSample: Sample = {
       index: index,
-      type: 'user',
+      type: "user",
       measurements: NUM_MEASUREMENTS,
       normOffsetX: normOffsetX,
-      normOffsetY: normOffsetY,
+      normOffsetY: height - height / 2,
       isHovered: false,
       moisture: moistureValues,
-      shear: shearValues
+      shear: shearValues,
     };
     // Add the new sample to the state
-    dispatch({ 
-      type: Action.ADD_SAMPLE, 
-      value: newSample 
-    }); 
+    dispatch({
+      type: Action.ADD_SAMPLE,
+      value: newSample,
+    });
     dispatch({
       type: Action.SET_USER_SAMPLE,
-      value: newSample
+      value: newSample,
     });
-    
-    dispatch({ 
-      type: Action.SET_DISABLE_SUBMIT_BUTTON, 
-      value: false 
+
+    dispatch({
+      type: Action.SET_DISABLE_SUBMIT_BUTTON,
+      value: false,
     });
-  }
+  };
 
   return (
-    <div id="clickable-image" style={{ position: 'relative' }} >
-      <img style={{height: '21vw', width: '44.5vw', margin: 'auto', display: 'block', boxShadow: '0px 2px 6px rgb(0 0 0 / 30%)', borderRadius: '4px'}}
-      ref={imgHeightRef} className={`${classes.imageDecision} ${enabled ? classes.cross : ''}`} id="pos-picker" src={diagram} onClick={onImageClick}/>
-      {
-        showNOMInput && <AddSamplePopup clickPosition={clickPosition} clickIndex={clickIndex} onAddData={addDataFunc} />
-      }
-      {
-        samples.map((sample, sampleIdx) => {
-          if (!imgEl) {
-            return null;
-          }
+    <div id="clickable-image" style={{ position: "relative" }}>
+      <img
+        ref={imgHeightRef}
+        className={`${classes.imageDecision} ${enabled ? classes.cross : ""}`}
+        id="pos-picker"
+        src={diagram}
+        onClick={onImageClick}
+      />
+      {showNOMInput && (
+        <AddSamplePopup
+          clickPosition={clickPosition}
+          clickIndex={clickIndex}
+          onAddData={addDataFunc}
+        />
+      )}
+      {samples.map((sample, sampleIdx) => {
+        if (!imgEl) {
+          return null;
+        }
 
-          const { index, type, normOffsetX, normOffsetY, isHovered } = sample;
+        const { index, type, normOffsetX, normOffsetY, isHovered } = sample;
 
-          // console.log("left: " + 23.82 * index + 82.53);
-          // console.log("top: " + String(11.62 * index + 1.15));
-          // console.log("\n")
-          
-          return <PositionIndicator
+        return (
+          <PositionIndicator
             key={sampleIdx}
-            left={23.82 * index + 82.53}
-            top={11.62 * index + 1.15}
-            // left={((index/INDEX_LENGTH)*701.1+196.9) * height / NORMALIZED_HEIGHT} //(samples[samples.length - 1].index*0.05)*701.1+196.9
-            // top={((index/INDEX_LENGTH)*(index/INDEX_LENGTH)*228.7 + (index/INDEX_LENGTH)*5.203+17.57) * height / NORMALIZED_HEIGHT} //(samples[samples.length - 1].index*0.05)*(samples[samples.length - 1].index*0.05)*228.7+(samples[samples.length - 1].index*0.05)*5.203+17.57
+            left={((NORMALIZED_FLAGATOB) * (index / INDEX_LENGTH) + NORMALIZED_STRAT) *  (height / NORMALIZED_HEIGHT)}
+            top={height - height / 2}
             rowIndex={sampleIdx}
             isHovered={isHovered}
             type={type}
             locationIndex={index}
             robot={false}
-          />;
-        })
-      }
+          />
+        );
+      })}
       {
         <PositionIndicatorRhex
-          left={23.82 * samples[samples.length - 1].index + 79.53}
-          top={11.62 * samples[samples.length - 1].index - 21.15}
-          // top={((samples[samples.length - 1].index/INDEX_LENGTH)*(samples[samples.length - 1].index/INDEX_LENGTH)*228.7+(samples[samples.length - 1].index*0.05)*5.203+17.57-30 ) * height / NORMALIZED_HEIGHT}//previous 7
-          // left={((samples[samples.length - 1].index/INDEX_LENGTH)*701.1+196.9 -10 ) * height / NORMALIZED_HEIGHT} //previous 50
+          left={
+            ((NORMALIZED_FLAGATOB) * (samples[samples.length - 1].index / INDEX_LENGTH) + NORMALIZED_STRAT) *  (height / NORMALIZED_HEIGHT)
+          }
+          top={height - height / 2 - 30}
         />
       }
-      {showRobotSuggestions && robotSuggestions &&
+      {showRobotSuggestions &&
+        robotSuggestions &&
         robotSuggestions.map((suggestion, rowIndex) => (
           <PositionIndicator
-            key={suggestion.index + suggestion.normOffsetX + suggestion.normOffsetY}
-            left={23.82 * suggestion.index + 82.53}
-            top={11.62 * suggestion.index + 1.15}
+            key={
+              suggestion.index + suggestion.normOffsetX + suggestion.normOffsetY
+            }
+            left={((NORMALIZED_FLAGATOB) * (suggestion.index / INDEX_LENGTH) + NORMALIZED_STRAT) *  (height / NORMALIZED_HEIGHT)}
+            top={height - height / 2}
             rowIndex={rowIndex}
             isHovered={suggestion.isHovered}
             type={suggestion.type}
             locationIndex={suggestion.index}
             robot={true}
           />
-        )) 
-      }
+        ))}
     </div>
   );
 }
