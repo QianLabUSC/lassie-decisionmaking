2d45935d72ad18c0f7470e6a416732de537a72ab
Nikola_Shrutika <shrutika051220@gmail.com>
2024-02-10 15:50:42 -0800

    add the logic of adding new sample paths when selected the options

--- src/components/RobotChart.tsx
+++ src/components/RobotChart.tsx
@@ -1,13 +1,12 @@
 import * as React from 'react';
-import { LinePath} from '@visx/shape';
+import { LinePath } from '@visx/shape';
 import { Text } from '@visx/text';
 import { Group } from '@visx/group';
 import { scaleLinear } from '@visx/scale';
 import { AxisLeft, AxisBottom } from '@visx/axis';
-import Typography from '@material-ui/core/Typography';
 import { useStateValue } from '../state';
 
-import { paths } from '../paths';
+// import { paths } from '../paths';
 
 const width = 1400;
 const height = 700;
@@ -17,9 +16,37 @@ interface Point {
   y: number;
 }
 
+export const newpath = [
+  [
+    [0.1, 0.2, 0.3, 0.4, 0.49],
+    [0.1,  0.115, 0.21, 0.32, 0.45]
+  ],
+  [
+      [0.1, 0.4, 0.47, 0.479, 0.48],
+      [0.1,  0.365, 0.465, 0.485, 0.49]
+  ],
+  [
+      [0.1, 0.243, 0.3543, 0.4359, 0.54],
+      [0.1,  0.3225, 0.345, 0.3832, 0.44]
+  ]
+];
 
-// Convert paths data to the format suitable for LinePath
-const lineData: Point[][] = paths.map((pathSet) =>
+const secondpath = [
+  [
+    [0.49, 0.510220440881763526, 0.610420841683366733, 0.71062124248496994, 0.89979959919839678, 0.89999999],
+    [0.45, 0.510220440881763526, 0.610420841683366733, 0.671062124248496994, 0.689979959919839678, 0.78978]
+  ],
+  [
+    [0.48, 0.499875, 0.535655, 0.56676, 0.587989, 0.75686787],
+    [0.49, 0.49879759519038075, 0.519899799599198396, 0.545465, 0.674556, 0.7898675]
+  ],
+  [
+    [0.54, 0.5501002004008016, 0.559020040080160321, 0.6503006012024048, 0.69989979959919839, .71],
+    [0.44, 0.5001002004008016032, 0.5250501002004008, 0.6541503006012024, 0.73386773547094188, 0.75]
+  ]
+];
+// Convert initial path data to the format suitable for LinePath
+const lineData: Point[][] = newpath.map((pathSet) =>
   pathSet[0].map((x, i) => ({ x, y: pathSet[1][i] }))
 );
 
@@ -34,16 +61,22 @@ const yScale = scaleLinear({
   range: [height - margin.bottom, margin.top],
 });
 
-// Labels for each path
+// Labels and Colors for each path
 const labels = ['A', 'B', 'C'];
+const colors = ['#FF5733', '#33FF57', '#3357FF']; // Example colors for 3 paths
 
-const RobotChart:React.FC = () => {
-  // This function determines if a path should be shown based on the selected option
+const RobotChart: React.FC = () => {
+  const [{ currUserStep }] = useStateValue();
 
-  const [{ currUserStep }] = useStateValue(); 
+  const getPathData = (index: number): Point[] => {
+    if (index < secondpath.length) {
+      return secondpath[index][0].map((x, i) => ({ x, y: secondpath[index][1][i] }));
+    }
+    return [];
+  };
 
   const shouldShowPath = (index: number): boolean => {
-    // If no option is selected (or if the "Reject suggestions" option is selected), show all paths
+    // If option D is selected (represented here by a special value like -1), show all paths
     if (currUserStep.acceptOrReject === -1 || currUserStep.acceptOrReject >= labels.length) {
       return true;
     }
@@ -51,38 +84,56 @@ const RobotChart:React.FC = () => {
     return index === currUserStep.acceptOrReject;
   };
 
-
   return (
     <svg width={width} height={height}>
       <Group>
         {lineData.map((data, index) => {
-          if (!shouldShowPath(index)) return null; // Do not render the path if it should not be shown
-
-          // Get the last point of the path for the label positioning
+          if (!shouldShowPath(index)) return null;  // Do not render the path if it should not be shown
+        
           const lastPoint = data[data.length - 1];
-          return (
-            <React.Fragment key={`line-group-${index}`}>
+
+          let pathsToRender = [
+            <React.Fragment key={`fragment-${index}`}>
               <LinePath
+                key={`line-${index}`}
                 data={data}
                 x={(d) => xScale(d.x)}
                 y={(d) => yScale(d.y)}
-                stroke={`#${Math.floor(Math.random() * 16777215).toString(16)}`} // Random color for each line
+                stroke={colors[index % colors.length]} // Use fixed color based on index
                 strokeWidth={4}
               />
-              {/* Add text label */}
               <Text
+                key={`text-${index}`}
                 x={xScale(lastPoint.x)}
                 y={yScale(lastPoint.y)}
-                dx={-30} // Offset a bit to the right
-                dy={25} // Offset a bit up
+                dx={-10} // Offset a bit to the right
+                dy={5} // Offset a bit up
                 fill="red"
-                fontSize={50}
+                fontSize={25}
                 fontWeight="bold"
               >
                 {labels[index]}
               </Text>
             </React.Fragment>
-          );
+          ];
+
+          // If the current path is selected, append the new path from secondpath
+          if (index === currUserStep.acceptOrReject) {
+            const newPathData = getPathData(index);
+            pathsToRender.push(
+              <LinePath
+                key={`line-new-${index}`}
+                data={newPathData}
+                x={(d) => xScale(d.x)}
+                y={(d) => yScale(d.y)}
+                stroke={colors[index % colors.length]} // Use the same fixed color for the new path
+                strokeWidth={4}
+                strokeDasharray="4,4" // Optional: makes the new path dashed
+              />
+            );
+          }
+
+          return pathsToRender;
         })}
         <AxisLeft scale={yScale} left={margin.left} />
         <AxisBottom scale={xScale} top={height - margin.bottom} />
@@ -90,6 +141,5 @@ const RobotChart:React.FC = () => {
     </svg>
   );
 };
-
 
 export default RobotChart;
